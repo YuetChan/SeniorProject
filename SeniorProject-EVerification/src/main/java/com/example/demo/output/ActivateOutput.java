9a1b6b3436deb49fdfc206c5ae4713e66cd56cd2
yuet <yuet@192.168.1.115>
2019-09-30 02:50:56 -0400

    first created

---
+++ SeniorProject-EVerification/src/main/java/com/example/demo/output/ActivateOutput.java
@@ -0,0 +1,29 @@
+package com.example.demo.output;
+
+public class ActivateOutput {
+	
+	private boolean success;
+
+	public ActivateOutput(boolean success) {
+		
+		super();
+		
+		this.success = success;
+		
+	}
+
+	public boolean isSuccess() {
+		
+		return success;
+		
+	}
+
+	public void setSuccess(boolean success) {
+		
+		this.success = success;
+		
+	}
+	
+	
+
+}
