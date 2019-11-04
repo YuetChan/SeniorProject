8667f12deda5cd4345d44273c059a2e6d60414c3
Yuet Chan <yuetchany@ufl.edu>
2019-11-04 02:47:39 -0500

    application name changed

--- EmojiVerificationBackend/src/main/java/com/example/demo/EmojiVerificationBackendApplication.java
+++ EmojiVerificationBackend/src/main/java/com/example/demo/EmojiVerificationBackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeniorProjectEVerificationApplication {
+public class EmojiVerificationBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeniorProjectEVerificationApplication.class, args);
+		
+		SpringApplication.run(EmojiVerificationBackendApplication.class, args);
+		
 	}
 
 }
