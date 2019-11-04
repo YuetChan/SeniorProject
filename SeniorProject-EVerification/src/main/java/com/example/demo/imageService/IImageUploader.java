75a3ca6dca03138d232aed6b0839f9a7a7230ae9
Yuet Chan <yuetchany@ufl.edu>
2019-11-03 20:33:44 -0500

    update project name from SeniorProject-EVerification to EmojiVerificationBackend

--- SeniorProject-EVerification/src/main/java/com/example/demo/imageService/IImageUploader.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.imageService;
-
-public interface IImageUploader {
-
-}
--- SeniorProject-EVerification/src/main/java/com/example/demo/mailService/IMailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.mailService;
-
-public interface IMailService {
-
-	public boolean sendMessage(String TO, String FROM, String SUBJECT, String TEXTBODY);
-	
-}
