8c56c2d0dae7a8030968a60d6a7b2ee3f67c2aba
Yuet Chan <yuetchany@ufl.edu>
2019-11-04 02:50:41 -0500

    application name changed

--- EmojiVerificationBackend/src/test/java/com/example/demo/EmojiVerificationBackendApplicationTests.java
+++ EmojiVerificationBackend/src/test/java/com/example/demo/EmojiVerificationBackendApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SeniorProjectEVerificationApplicationTests {
+public class EmojiVerificationBackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
